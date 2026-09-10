f739ed9d598ee99bec81443c4c2bbf0c996b68b4
MonkeHlam <vecktor887@gmail.com>
2026-09-11 01:25:27 +0700

    fix: canonicalize application router

---
+++ frontend/src/app/router/index.js
@@ -0,0 +1,9 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import { ProjectsPage } from '@/pages'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [{ path: '/projects', name: 'projects', component: ProjectsPage }],
+})
+
+export default router
